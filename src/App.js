8c46fa5432ccf6416d9f03afd3c61f5b10a37074
JMDahle <47642064+jmdahle@users.noreply.github.com>
2020-02-16 15:57:14 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignupForm from './components/sign-up';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>React-Passport Test</h1>
+                </header>
+                <SignupForm />
+            </div>
+        );
+    }
 }
 
 export default App;
